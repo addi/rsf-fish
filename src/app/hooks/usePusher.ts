7b10d7761b7bf66cba239760f72cbb90e450a311
Árni Jónsson <addininja@gmail.com>
2024-11-05 16:54:30 +0100

    Update usePusher.ts

--- src/app/hooks/usePusher.ts
+++ src/app/hooks/usePusher.ts
@@ -10,8 +10,16 @@ const usePusherBids = (channelName: string) => {
   };
 
   useEffect(() => {
+    const key = process.env.NEXT_PUBLIC_PUSHER_APP_KEY || "enginn key";
+
+    console.log("Connecting to pusher", key);
+
     const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY || "", {
       cluster: "eu",
+    });
+
+    pusher.connection.bind("connected", () => {
+      console.log("Connected to pusher");
     });
 
     const channel = pusher.subscribe(channelName);
